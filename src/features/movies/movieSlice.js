0da3879dd776fb313dbc8bd3e823421342c53b1b
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-30 20:17:19 -0300

    Importar createSlice de reduxjs-toolkit e criar o slice para o projeto de filmes. Definir initialState e a action addMovies. criar funcao getAllMovies e importar o reducer criado na store.

---
+++ src/features/movies/movieSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+// params
+// 1. slice name 2. initial state 
+// 3. reducers 4. extra reducers (optional)
+const initialState = {
+	movies: []
+};
+
+
+const movieSlice = createSlice({
+	name: 'movies',
+	initialState,
+	reducers: {
+		addMovies: (state, { payload }) => {
+			state.movies = payload;
+		},
+	},
+});
+
+
+
+export const { addMovies } = movieSlice.actions;
+
+export const getAllMovies = (state) => state.movies.movies;
+
+export default movieSlice.reducer;
+
+
+
--- src/features/store.js
+++ src/features/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+// reducers
+import moviesReducer from './movies/movieSlice';
+
 
 
 export const store = configureStore({
-	reducer: {},
+	reducer: moviesReducer,
 });
 
 
