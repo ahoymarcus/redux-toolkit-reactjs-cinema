451d5e5314e84b874683de523892f1cc2bdbb834
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-30 19:36:35 -0300

    Criar modulo store.js e importar o objeto configureStore de @reduxjs-tollkit. Exportar a store ainda sem reducer definido e importar em index.js, juntamente com o componente Provider de react-redux.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+import { Provider } from 'react-redux';
+import { store } from './features/store';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-		<App />
+		<Provider store={store} >
+			<App />
+		</Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
