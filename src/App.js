747d98c8ee20c7fd518285351f419a091b1fd5e0
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-31 17:17:49 -0300

    Atualizar estrutura Jsx do MovieCard.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // https://www.youtube.com/watch?v=2kL28Qyw9-0
-//  0hs  57' 00''
+//  1hs  02' 00''
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // styles
